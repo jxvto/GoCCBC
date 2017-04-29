fb015b99d7632d1710e2465e1770b5e1717cb9be
Ernest Essuah Mensah <Ernest@Ernests-MacBook-Pro.local>
2017-04-28 23:03:06 -0400

    -> fixes issue in last commit

---
+++ GoCCBC/ChallengeScreenVC.swift
@@ -0,0 +1,32 @@
+//
+//  ChallengeScreenVC.swift
+//  GoCCBC
+//
+//  Created by Ernest Essuah Mensah on 4/24/17.
+//  Copyright © 2017 Ernest Essuah Mensah. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeScreenVC: UIViewController {
+    
+    @IBOutlet weak var mapLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var beginButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        beginButton.layer.cornerRadius = 10
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        imageView.image = (presentingViewController as! MapViewController).imageView.image
+        mapLabel.text = (presentingViewController as! MapViewController).selectedMap.text
+    }
+
+
+}
